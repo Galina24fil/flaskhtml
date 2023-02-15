e3f8d59f538583d5f7df9a9e6b59866d71c2b680
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-02-15 23:15:05 +0300

    Загрузка файла
    Загрузка файла

--- main.py
+++ main.py
@@ -1,76 +1,67 @@
-from flask import Flask, url_for
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "Миссия Колонизация Марса"
-
-
-@app.route('/index')
-def index1():
-    return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/promotion')
-def promotion():
-    countdown_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                      'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
-    return '</br>'.join(countdown_list)
-
-
-@app.route('/image_mars')
-def return_sample_page():
-    return """<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="/static/mars.jpg" alt="здесь должна была быть картинка, но не нашлась">
-                    <p>Вот она какая, красная планета</p>
-                  </body>
-                </html>"""
-
-
-@app.route('/promotion_image')
-def bootstrap():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Колонизация</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="/static/mars.jpg" alt="здесь должна была быть картинка, но не нашлась">
-                    <div class="alert alert-secondary" role="alert">
-                      Человечество вырастает из детства.
-                    </div>
-                    <div class="alert alert-success" role="alert">
-                      Человечеству мала одна планета.
-                    </div>
-                    <div class="alert alert-light" role="alert">
-                      Мы сделаем обитаемыми безжизненные пока планеты.
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                      И начнем с Марса!
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                      Присоединяйся!
-                    </div>
-                  </body>
-                </html>'''
+@app.route('/load_photo', methods=['POST', 'GET'])
+def astronaut_selection():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример формы</title>
+                          </head>
+                          <body>
+                            <h1>Загрузка фотографии</h1>
+                            <h3>для участия в миссии</h3>
+                            <div>
+                                <form class="login_form" method ="post">
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label> 
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        a = request.form['file']
+        return f'''<!doctype html>
+                                <html lang="en">
+                                  <head>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                    <link rel="stylesheet"
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                    crossorigin="anonymous">
+                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                                    <title>Отбор астронавтов</title>
+                                  </head>
+                                  <body>
+                                    <h1>Загрузка фотографии</h1>
+                                    <h3>для участия в миссии</h3>
+                                    <div>
+                                        <form class="login_form" method ="post">
+                                            <div class="form-group">
+                                                <label for="photo">Приложите фотографию</label> 
+                                                <input type="file" class="form-control-file" id="photo" name="file">
+                                            </div>
+                                            </br>
+                                            <img src="/static/{a}" class="d-block w-100" alt="Картинка не открылась...">
+                                            <button type="submit" class="btn btn-primary">Отправить</button>
+                                        </form>
+                                    </div>
+                                  </body>
+                                </html>'''
 
 
 if __name__ == '__main__':
