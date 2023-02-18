e65caea5f36dd4165943e4b6ba25880b417b06b7
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-02-18 14:14:27 +0300

    Добавляем капитана
    Добавляем капитана

--- main.py
+++ main.py
@@ -1,77 +1,28 @@
-from flask import Flask, url_for
+from flask import Flask
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "yandexluceym_secret_key"
 
 
-@app.route('/')
-def index():
-    return "Миссия Колонизация Марса"
+def main():
+    db_session.global_init("db/blogs.db")
+    db_sess = db_session.create_session()
+    user = User(surname='Scott', name='Ridley', age=21, position='captain',
+                speciality='research engineer', address='module_1', email='scott_chief@mars.org')
+    db_sess.add(user)
+    for n in range(2, 5):
+        user = User()
+        user.name = f"Пользователь {n}"
+        user.about = f"Что то о пользователе {n}"
+        user.email = f"user{n}@mail.ru"
+        db_sess.add(user)
+    db_sess.commit()
+    for user in db_sess.query(User).all():
+        print(user.name, user.email)
+    app.run()
 
 
-@app.route('/index')
-def index1():
-    return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/promotion')
-def promotion():
-    countdown_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                      'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
-    return '</br>'.join(countdown_list)
-
-
-@app.route('/image_mars')
-def return_sample_page():
-    return """<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="/static/mars.jpg" alt="здесь должна была быть картинка, но не нашлась">
-                    <p>Вот она какая, красная планета</p>
-                  </body>
-                </html>"""
-
-
-@app.route('/promotion_image')
-def bootstrap():
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Колонизация</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="/static/mars.jpg" alt="здесь должна была быть картинка, но не нашлась">
-                    <div class="alert alert-secondary" role="alert">
-                      Человечество вырастает из детства.
-                    </div>
-                    <div class="alert alert-success" role="alert">
-                      Человечеству мала одна планета.
-                    </div>
-                    <div class="alert alert-light" role="alert">
-                      Мы сделаем обитаемыми безжизненные пока планеты.
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                      И начнем с Марса!
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                      Присоединяйся!
-                    </div>
-                  </body>
-                </html>'''
-
-
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+if __name__ == "__main__":
+    main()
