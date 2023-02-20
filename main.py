93a1fc250c1901a3f399419f064f399fd18e19f3
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-02-20 14:13:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,5 @@ def index(username):
     return render_template('index.html', title=username)
 
 
-@app.route('/list_prof/<list>')
-def index2(list):
-    return render_template('index.html', a=list)
-
-
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
