7389c8034da638557d0d492123c845b077581a45
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-02-20 14:13:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index1():
+    return render_template('index.html', title='Главная страница')
+
+
+@app.route('/index/<username>')
+def index(username):
+    return render_template('index.html', title=username)
+
+
 @app.route('/list_prof/<list>')
-def index(list):
-    if list == 'ol':
-        return render_template('index.html', ol=1, ul=0)
-    if list == 'ul':
-        return render_template('index.html', ul=1, ol=0)
+def index2(list):
+    return render_template('index.html', a=list)
 
 
 if __name__ == '__main__':
