3d8ccff0c27821b23d414e6b97367b8b386b4ebb
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-02-20 14:05:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index1():
+    return render_template('index.html', title='Главная страница')
+
 @app.route('/index/<username>')
 def index(username):
     return render_template('index.html', title=username)
