8e748a0ec5676ad54c9a75353c37d611d44632cb
Galina24fil <118717385+Galina24fil@users.noreply.github.com>
2023-02-21 18:58:08 +0300

    Загрузка файла
    Загрузка файла

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, url_for
-
 app = Flask(__name__)
 
 
 @app.route('/load_photo', methods=['POST', 'GET'])
 def astronaut_selection():
     if request.method == 'GET':
-        return f'''<!doctype html>
+        return '''<!doctype html>
                         <html lang="en">
                           <head>
                             <meta charset="utf-8">
@@ -15,8 +14,8 @@ def astronaut_selection():
                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                             crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пример формы</title>
+                            <link rel="stylesheet" type="text/css" href="static/css/style.css" />
+                            <title>Отбор астронавтов</title>
                           </head>
                           <body>
                             <h1>Загрузка фотографии</h1>
@@ -25,43 +24,17 @@ def astronaut_selection():
                                 <form class="login_form" method ="post">
                                     <div class="form-group">
                                         <label for="photo">Приложите фотографию</label> 
-                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                        <input type="file" class="form-control-file" id="photo" name="file" onchange="mars.src = window.URL.createObjectURL(this.files[0])">
                                     </div>
+                                    <img id="mars" src="" class="img-fluid" />
+                                    <br/>
                                     <button type="submit" class="btn btn-primary">Отправить</button>
                                 </form>
                             </div>
                           </body>
                         </html>'''
     elif request.method == 'POST':
-        a = request.form['file']
-        return f'''<!doctype html>
-                                <html lang="en">
-                                  <head>
-                                    <meta charset="utf-8">
-                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                    <link rel="stylesheet"
-                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                    crossorigin="anonymous">
-                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                                    <title>Отбор астронавтов</title>
-                                  </head>
-                                  <body>
-                                    <h1>Загрузка фотографии</h1>
-                                    <h3>для участия в миссии</h3>
-                                    <div>
-                                        <form class="login_form" method ="post">
-                                            <div class="form-group">
-                                                <label for="photo">Приложите фотографию</label> 
-                                                <input type="file" class="form-control-file" id="photo" name="file">
-                                            </div>
-                                            </br>
-                                            <img src="/static/{a}" class="d-block w-100" alt="Картинка не открылась...">
-                                            <button type="submit" class="btn btn-primary">Отправить</button>
-                                        </form>
-                                    </div>
-                                  </body>
-                                </html>'''
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
